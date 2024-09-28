c913ac95238e2225d6e65016e8f3100bbec1563b
Zsolti0715 <samatron02@gmail.com>
2024-09-28 13:59:56 +0200

    házifeladat

---
+++ main.py
@@ -0,0 +1,16 @@
+import e11
+import e13
+import e14
+import e15
+import e5
+import e6
+import e7
+import e11
+
+#e6.szog()
+#e7.negyzetgyok()
+#e13.kor()
+#e14.kocka()
+#e15.teglalap()
+#e5.honap()
+e11.husvet()
